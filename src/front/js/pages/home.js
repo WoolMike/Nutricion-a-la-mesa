d896e875725388a5e6e98a9ce326640eee1ad940
AngelVr-a <angelverar2001@gmail.com>
2024-08-31 00:51:51 +0000

    Recetas-1

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import imagenejemplo from "../../img/how-to.png";
+
 import "../../styles/home.css";
 
 export const Home = () => {
